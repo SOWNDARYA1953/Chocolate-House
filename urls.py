4f8cc1885714b8510a68064d4585623c0cb0de09
Sowndaryahb <soundaryahb20@gmail.com>
2024-11-05 16:24:23 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,27 +1,10 @@
-"""
-URL configuration for chocolate_house project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# chocolate_house/urls.py
-from django.contrib import admin
-from django.urls import path, include
-from inventory import views as inventory_views  # Import the homepage view
-
+# inventory/urls.py
+from django.urls import path
+from . import views 
+from .views import ThankYouView
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('inventory/', include('inventory.urls')),  # Existing routes for inventory app
-    path('', inventory_views.homepage, name='homepage'),  # Homepage route
-   
+    path('flavors/', views.flavor_list, name='flavor_list'),
+    path('ingredients/', views.ingredient_list, name='ingredient_list'),
+    path('suggest/', views.suggest_flavor, name='suggest_flavor'),
+    path('thank_you/', views.ThankYouView.as_view(), name='thank_you'),
 ]
